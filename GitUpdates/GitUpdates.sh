51d160a1aa36354bac33544cc56d00618ba544a5
Jersi132 <30377539+Jersi132@users.noreply.github.com>
2019-05-06 15:18:24 +0100

    Update GitUpdates.sh

--- GitUpdates/GitUpdates.sh
+++ GitUpdates/GitUpdates.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cd /home/pi/$1
-git pull
-npm install
+filename='gitlist.txt'
+#echo Start
+while read p; do 
+    cd /home/pi/$p
+    git pull
+    npm install
+done < $filename
+
+#cd /home/pi/$1
+#git pull
+#npm install
 
