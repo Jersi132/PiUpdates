c0888add61dc32bc2b7a7cbece74bbef8000636e
Jersi132 <30377539+Jersi132@users.noreply.github.com>
2019-05-06 15:41:51 +0100

    Update MainUpdates.sh
    Added Github Updates

--- MainUpdates.sh
+++ MainUpdates.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Github Updates
+cd /home/pi/PiUpdates/GitUpdates
+GitUpdates.sh
+
+#System Updates
 sudo apt-get update && sudo apt-get dist-upgrade -y 
 sudo rpi-update -y
 sudo apt-get autoremove -y
