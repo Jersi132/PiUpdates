cb81e81b12e9860199dfc8c0d9350baa43d78ae0
Jersi132 <30377539+Jersi132@users.noreply.github.com>
2019-05-09 14:57:23 +0100

    Update MainUpdates.sh

--- MainUpdates.sh
+++ MainUpdates.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+currentDate=`date`
+echo "Start: "$currentDate
+
 #Github Updates
 cd /home/pi/PiUpdates/GitUpdates && ./GitUpdates.sh
 
@@ -9,3 +12,7 @@ sudo rpi-update -y
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
 sudo reboot
+
+currentDate=`date`
+echo "Finish: "$currentDate
+
